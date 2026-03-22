2ffc075c1fa40d09535fc7e5fbedfd710e173c57
Priyanshu Vishwakarma <priyanshuvk2005@gmail.com>
2026-03-22 12:32:59 +0530

    add readme file

---
+++ src/main/java/com/smartcv/builder/config/DotenvConfig.java
@@ -0,0 +1,16 @@
+package com.smartcv.builder.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class DotenvConfig {
+
+    @Bean
+    public Dotenv dotenv() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        return dotenv;
+    }
+}
